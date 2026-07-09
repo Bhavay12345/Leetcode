25da5f1c4acb203ab735cd28402373cf84290f8a
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-07-10 01:32:07 +0530

    Time: 514 ms (99.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,11 @@
+select ROUND(sum(tiv_2016),2) as tiv_2016 from Insurance
+where tiv_2015 in (
+    select tiv_2015 from Insurance
+    group by tiv_2015
+    having COUNT(*)>1
+)
+AND (lat,lon) in (
+    select lat,lon from Insurance
+    group by lat,lon
+    having COUNT(*)=1
+);
