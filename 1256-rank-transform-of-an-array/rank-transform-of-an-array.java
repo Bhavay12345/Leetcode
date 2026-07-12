54de1a925d7fc06bd2b78c5ee54eb1f8766f12e5
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-07-12 20:07:39 +0530

    Time: 38 ms (28.89%) | Memory: 66 MB (99.46%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+        if(n==0 ) return arr;
+        if(n==1){
+            arr[0]=1;
+            return arr;
+        } 
+        int[][] sortarr=new int[n][2];
+        int rank=1;
+        for(int i=0;i<n;i++){
+            sortarr[i][0]=arr[i];
+            sortarr[i][1]=i;
+        }
+        Arrays.sort(sortarr,(a,b)-> Integer.compare(a[0],b[0]));
+        arr[sortarr[0][1]]=rank; 
+        for(int i=1;i<n;i++){
+            if(sortarr[i][0]==sortarr[i-1][0]){
+                arr[sortarr[i][1]]=rank;
+            }
+            else{
+                arr[sortarr[i][1]]=++rank;
+            }
+            
+        }
+        return arr;
+    }
+    
+}      
