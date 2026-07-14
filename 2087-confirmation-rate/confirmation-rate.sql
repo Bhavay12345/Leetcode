d6b934514e2b3bdc0632a5ce352358ca1dc5ba6b
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-07-15 02:17:14 +0530

    Time: 628 ms (94.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,7 @@
+select 
+    s.user_id,
+    IFNULL(ROUND(SUM(c.action='confirmed')/COUNT(c.action),2),0) as confirmation_rate
+from Signups as s
+left join Confirmations as c
+on s.user_id=c.user_id
+group by s.user_id;
