59a69fe9f3d7f34666fad09f4aa814814f788d0c
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-08-02 02:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (41.35%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        Integer[][] dp=new Integer[n][n];
         int sum=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             sum+=nums[i];
         }
-        int sum1=fxn(0,nums.length-1,nums); int sum2=sum-sum1;
+        int sum1=fxn(0,n-1,nums,dp); int sum2=sum-sum1;
         if(sum1>=sum2) return true;
         else return false;
     }
-    public static int fxn(int i,int j,int[] nums){
+    public static int fxn(int i,int j,int[] nums,Integer[][] dp){
         if(i>j) return 0;
         if(i==j) return nums[i];
-        int take_i=nums[i]+ Math.min(fxn(i+2,j,nums),fxn(i+1,j-1,nums));
-        int take_j=nums[j]+ Math.min(fxn(i+1,j-1,nums),fxn(i,j-2,nums));
+        if(dp[i][j]!=null) return dp[i][j];
+        int take_i=nums[i]+ Math.min(fxn(i+2,j,nums,dp),fxn(i+1,j-1,nums,dp));
+        int take_j=nums[j]+ Math.min(fxn(i+1,j-1,nums,dp),fxn(i,j-2,nums,dp));
 
-        return Math.max(take_i,take_j);
+        return dp[i][j]=Math.max(take_i,take_j);
     }
 }
