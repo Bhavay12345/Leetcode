6397c901055d726ab04045ed42e78cc8753f8453
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-08-20 22:02:05 +0530

    Time: 52 ms (79.09%) | Memory: 116.2 MB (81.69%) - LeetSync

---
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        int n=heights.length;
+        int[] ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            int count=0;
+            while(!st.isEmpty() && heights[i]>=st.peek()){
+                st.pop();
+                count++;
+            } 
+            // First taller person is also visible
+            if(!st.isEmpty()) count++;
+            ans[i]=count;
+            st.push(heights[i]);
+        }
+        return ans;
+    }
+}
