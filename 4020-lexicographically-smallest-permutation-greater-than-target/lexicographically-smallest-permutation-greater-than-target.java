3f026d23d9ac149d3cc88fc6943100f7d7f8fed0
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-08-31 15:40:54 +0530

    Time: 3 ms (55.46%) | Memory: 44.6 MB (94.38%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.java
@@ -1,37 +1,43 @@
 class Solution {
-    String result = "";
+    String res="";
     public String lexGreaterPermutation(String s, String target) {
-        int[] count = new int[26];
-        for (char ch : s.toCharArray())
-            count[ch - 'a']++;
-        StringBuilder curr = new StringBuilder();
-        solve(curr, count, target, 0, false);
-
-        return result;
+        int[] count= new int[26];
+        for(int i=0;i<s.length();i++){
+            int num=s.charAt(i)-'a';
+            count[num]++;
+        }
+        StringBuilder ans=new StringBuilder();
+        fxn(0,ans,target,count,false);
+        return res;
     }
-    boolean solve(StringBuilder curr, int[] count, String target, int i, boolean greater) {
-        if (i == target.length()) {
-            if (greater) {
-                result = curr.toString();
+    public boolean fxn(int i,StringBuilder ans,String t,int[] count,boolean greater){
+        //base case
+        if(i >= t.length()){
+            if(greater==true){
+                res=ans.toString();
                 return true;
             }
             return false;
         }
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            if (count[ch - 'a'] == 0)
-                continue;
-            if (greater == false && ch < target.charAt(i))
-                continue;
-            curr.append(ch);
-            count[ch - 'a']--;
+        
+        for(char ch='a' ;ch <= 'z';ch++){
+            if(count[ch - 'a'] <= 0) continue; //that char is not aval
+            if(greater==false && ch < t.charAt(i)) continue; //abe curr t char se chota ni le sakte 
 
-            boolean isGreater = greater || ch > target.charAt(i);
-            if (solve(curr, count, target, i + 1, isGreater)) {
+            // Append char
+            ans.append(ch);
+            count[ch-'a']--;
+//mark greater T if greater allready T || added grater ch in ans
+            boolean isGreater= greater || ch > t.charAt(i);
+
+            // move forward
+            if(fxn(i+1,ans,t,count,isGreater)==true){ // last m smallest lexoco mil gya
                 return true;
             }
-            curr.deleteCharAt(curr.length() - 1);
-            count[ch - 'a']++;
+            // Back track (last tak greater true hi nhi hua)
+            ans.deleteCharAt(ans.length()-1);
+            count[ch-'a']++;
         }
-        return false;
+        return false; // s = "baba", target = "bbaa"
     }
 }
