352f6e4d0e5a85b6267dbd85aa51506680458cd3
Bhavay Mohvia <72340885+Bhavay12345@users.noreply.github.com>
2026-09-09 01:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (40.03%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,8 +1,6 @@
 class Solution {
     public int countCommas(int n) {
         if(n<=999) return 0;
-        String s= String.valueOf(n);
-        int len = s.length();
         return n-999;
     }
 }
